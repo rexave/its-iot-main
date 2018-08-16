5691df48cea108fc068ac7f566fec283b6050e09
Rexave <rexave@gmail.com>
2018-08-16 20:50:21 +0200

    watchgod for 3G connection

---
+++ 3Gwatchdog.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true
+do
+    ping -c 5 -I ppp0 google.com > /tmp/wvdialchecker
+    RC=`grep "5 received" /tmp/wvdialchecker | wc -l`
+    if [ $RC -eq 0 ];
+    then
+        echo ------------------------------
+        echo 3G lost at
+        date
+        echo try to reconnect
+        sudo pkill wvdial
+        sudo wvdial 3gconnect&
+        sleep 90
+        /home/pi/config_3G.sh &
+        echo ------------------------------
+    fi
+    sleep 300
+done
