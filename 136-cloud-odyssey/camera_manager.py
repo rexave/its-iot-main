af23faaac95dfa498eb51186f499c6b773b1cf06
Amael HEILIGTAG <moi@kerkristal.com>
2018-08-29 08:30:52 +0200

    Change logs + change filename format : put incremental number in front of filename and keep last value even if program restarted. All pictures will thus keep their order even if local time is lost after a reboot

--- 136-cloud-odyssey/camera_manager.py
+++ 136-cloud-odyssey/camera_manager.py
@@ -12,7 +12,7 @@ from picamera import PiCamera
 filename = "/home/pi/136-cloud-odyssey/common_functions.py"
 exec(open(filename).read())
 
-log("Script start")
+logNotice("Script start")
 
 #################################################"
 # Global variables
@@ -27,7 +27,7 @@ camera.flash_mode = 'off'
 camera.rotation = 180
 
 # Create directory if not existing
-log("creating directory " + str(dir_home))
+logInfo("creating directory " + str(dir_home))
 try:
     os.stat(dir_home)
 except:
@@ -36,16 +36,25 @@ except:
 # Start all sequences at 1
 i = 1
 
+file_list = os.listdir(dir_home)
+if not file_list:
+    latest_file = ""
+else:
+    latest_file = max(file_list, key=lambda x:x[:+13])
+    i = int(latest_file[6:12]) + 1
+
+logDebug("Info i = " + str(i) + " last file :" + latest_file[6:12])
+
 # Check if enough free space on SD Card
 def check_free_storage():
     statvfs = os.statvfs("/home/pi")
     Gb_free_on_storage = statvfs.f_bsize * statvfs.f_bavail / 1024 / 1024 / 1024
     if Gb_free_on_storage < 2:
-        log("insufficent free storage : " + str(Gb_free_on_storage) + " Gb")
-        log("exiting to protect the system")
+        logWarning("insufficent free storage : " + str(Gb_free_on_storage) + " Gb")
+        logError("exiting to protect the system")
         exit(0)
 
-log("Begin loop")
+logInfo("Begin loop")
 
 # Main loop
 while True:
@@ -57,8 +66,8 @@ while True:
     file_name = (str(current_time[0]) + "-" + str(current_time[1]).zfill(2) + "-" + str(current_time[2]).zfill(2) + "_" +
             str(current_time[3]).zfill(2) + "-" + str(current_time[4]).zfill(2) + "-" + str(current_time[5]).zfill(2) )
 
-    camera.capture(dir_home + '/image_' + file_name + '_%05d.jpg' % i)
-    log("Photo saved " + dir_home + '/image_' + file_name + '_%05d.jpg' % i)
+    camera.capture(dir_home + '/image_%06d_' %i + file_name + '.jpg')
+    logDebug("Photo saved " + dir_home + '/image_%06d_' %i + file_name + '.jpg')
 	
     if i % 10 == 0:
         check_free_storage()
--- 136-cloud-odyssey/uploadPictures.sh
+++ 136-cloud-odyssey/uploadPictures.sh
@@ -5,29 +5,31 @@
 ################################################################################
 # 
 
+#set -x
+
 # Include common functions
 . ~/136-cloud-odyssey/common_functions.sh
-# Reinitialize log file
-init_log
 
 # Start script
-log "Script start"
+logNotice "Script start"
 
 PICTUREFOLDER=/home/pi/camera/photos
 
 while true
 do 
-	# Try to send ALL unsent files until now (one out of 20)
-	for filename in $(ls -tr $PICTUREFOLDER/*[0,2,4,6,8]0.jpg 2>/dev/null) ; do
-		#log "Uploading : " ${filename}
+	#ls -l ${PICTUREFOLDER}/image_*[0,2,4,6,8]0_*.jpg | head -n -1 | 2>&1
+	#echo "test"
+	# Try to send ALL unsent files until now (one out of 20) except last one which could still be written
+	for filename in $(ls ${PICTUREFOLDER}/image_*[0,2,4,6,8]0_*.jpg | head -n -1) ; do
+		#logDebug "Uploading : " ${filename}
 
 		# Capture and log result
 		vl_result=$(/home/pi/dropboxUploader/dropbox_uploader.sh upload ${filename} $(echo $(basename ${filename}) | sed "s/.jpg/__sent_$(date +%F"_"%H-%M-%S).jpg/") && mv ${filename} ${filename}".sent")
 		vl_ret=$?
-		log ${vl_result} " with return code $vl_ret"
+		logInfo ${vl_result} " with return code $vl_ret"
 	done
-	# wait betwen each loop so that there sould always be at least one file available
+	logInfo "wait betwen each loop so that there sould always be at least one file available"
 	sleep 10
 done
 
-log "Script end"
+logNotice "Script end"
